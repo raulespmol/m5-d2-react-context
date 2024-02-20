c46c9f1d14d9e8ddc527977cc9329be500e140b4
Raul Espinoza M <raul.espmol@gmail.com>
2024-02-20 00:17:58 -0300

    alternar like al clickear

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,11 +4,28 @@ import { PicturesContext } from "../PicturesContext";
 const Gallery = () => {
   const {pictures, setPictures} = useContext(PicturesContext)
 
+  const toggleFav = (id) => { 
+    const newPictures = pictures.map(picture => {
+      if(picture.id === id){
+        const isLiked = picture.liked
+        return {...picture, liked: !isLiked}
+      }
+      return picture
+    })
+    setPictures(newPictures)
+  }
+
   return(
     <div className="gallery grid-columns-5 p-3">
       {pictures.map(pic => {
         return (
-          <img src={pic.src.small} alt="" key={pic.id}/>
+          <img 
+            className="photo"
+            src={pic.src.medium} 
+            alt="pic.alt" 
+            key={pic.id}
+            onClick={() => toggleFav(pic.id)}
+          />
         )
       })}
     </div>
