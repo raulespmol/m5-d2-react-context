fcbbaaa74d0544fc7a60bf2985bac506360e4e8a
Raul Espinoza M <raul.espmol@gmail.com>
2024-02-20 18:54:20 -0300

    ajustes estilos navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
+import 'boxicons'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar flex">
       <Link to="/"><h1>NaturalPic</h1></Link>
       <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/favoritos">Favoritos</Link>
+          <Link to="/" className="flex">
+            <box-icon name='home' type='solid' color='#ffffff' ></box-icon>
+            Home
+          </Link>
+          <Link to="/favoritos" className="flex">
+            <box-icon name='heart' type='solid' color='#ffffff' ></box-icon>
+            Favoritos
+          </Link>
       </div>
     </nav>
   );
